49da4ffa3aaa83537e1b57be076d39ab61623471
David Derr <david.derr@stud.hs-hannover.de>
2024-02-28 11:26:18 +0100

    style: MonthSelection Popover opens on click of TextField

--- src/components/MonthSelection.js
+++ src/components/MonthSelection.js
@@ -1,21 +1,29 @@
-import { TextField } from '@mui/material';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { useState } from 'react';
 
 export default function MonthSelection({ date, setDate }) {
+    const [open, setOpen] = useState(false);
+
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DatePicker
+                onClose={() => setOpen(false)}
+                open={open}
                 openTo="month"
                 views={['year', 'month']}
                 value={date}
                 onChange={(newDate) => {
+                    setOpen(false);
                     setDate(newDate);
                 }}
                 slotProps={{
                     textField: {
+                        onClick: () => setOpen(true),
                         variant: 'outlined',
                         inputProps: {
+                            onMouseDown: (e) => e.preventDefault(),
+                            readOnly: true,
                             style: {
                                 textAlign: 'center'
                             }
